cc720e3259750ea14ac69173df412b9397526cbe
ARodriguez97 <4800.joan23@fje.edu>
2017-01-30 15:39:49 +0100

    Added Style for register
    Register Log CSS

--- Proyecto 5 -- V1/php/alta.php
+++ Proyecto 5 -- V1/php/alta.php
@@ -3,9 +3,10 @@
 <html>
 <head>
 	<title>MAMProyect</title>
-	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/costum.css">
 	    <script>
-    //Funcion para validar  el formulario 
+    //Funcion para validar  el formulario
     function validar()
     {
       var error ="";
@@ -20,6 +21,12 @@
         error+="Error, la contraseña no puede estar vacio \n";
         document.getElementById("pass").style.borderColor="red";
       }
+			p1 = document.getElementById("pass").value;
+			p2 = document.getElementById("pass2").value
+			if(p1 != p2){
+				error+="Error, la contraseña no coincide \n";
+				document.getElementById("pass2").style.borderColor="red";
+			}
       if(error!="")
       {
         alert(error);
@@ -34,22 +41,32 @@
     </script>
 </head>
 <body>
+
+	<div class="login_general">
+		<p class="titulo_mc">My Contacts</p>
+	<div class="login_marco">
 	<div class="login_logo">
-		<img  class=" login_image" src="img/logo.jpg">
+		<img  class=" login_image" src="../img/logo.png">
 	</div>
-		
 	<form name="login" action="alta.proc.php" method="POST" onsubmit="return validar();" >
 		<div class="login">
 			<p class="nickname">
-				Introduce un nombre de usuario:<br/>
+				Nombre de usuario:
 				<input type="text" class="name" name="name" id="name" maxlength="15" />
 			</p>
 			<p class="password">
-				Introduce una contraseña: <br/>
+				Contraseña:
 				<input type="password" class="pass" name="pass" id="pass" />
 			</p>
+			<p class="password">
+				Repetir contraseña:
+				<input type="password" class="pass" name="pass2" id="pass2"/>
+			</p>
+			<p class="register_login">Ya estas registrado? Inicia sesión haciendo <a href="php/alta.php">click aquí<a></p>
 			<input class="send" id="send" type="submit" value="Acceder" />
 		</div>
 	</form>
-
+</div>
+</div>
 </body>
+</html>
