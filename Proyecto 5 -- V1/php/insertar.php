6a0aaa8c657975dc0990a8be1430c9e3b55cad80
ARodriguez97 <4800.joan23@fje.edu>
2017-01-31 18:23:50 +0100

    Css insertar

--- Proyecto 5 -- V1/php/insertar.php
+++ Proyecto 5 -- V1/php/insertar.php
@@ -46,13 +46,13 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">My Contacts</a>
+                <img class="logo_header" src="../img/logo.png" href="#"><a href='#' style="padding-left:10px;">My Contacts</a></img>
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
 
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>  <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li>
                             <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
@@ -76,7 +76,7 @@
         </nav>
 
         <div id="page-wrapper" class="page-wrapper">
-            <div class="container-fluid">
+            <div class="container-fluid container-fluid2">
                 <div class="row ">
                     <h2 style="text-align:center;">Añadir Contacto</h2>
                   <form name="formulario_ins" class="formulario_ins" method="post" action="send_form_email.php">
@@ -125,7 +125,7 @@
                   </tr>
                   <tr>
                    <td colspan="2" style="text-align:center">
-                    <input type="submit" value="Enviar">   <a href="http://www.tufelicidadvacacional.com.ve/email_form.php"></a>
+                    <input type="submit" class="submit" value="Enviar">   <a href="http://www.tufelicidadvacacional.com.ve/email_form.php"></a>
                    </td>
                   </tr>
                   </table>
